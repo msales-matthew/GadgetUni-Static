006b8e5ecc3522164553410471b12952541f6999
msales-matthew <32294965+msales-matthew@users.noreply.github.com>
2017-12-05 13:49:07 +0100

    ads hide/show

--- themes/forty/static/js/functions.js
+++ themes/forty/static/js/functions.js
@@ -1,6 +1,9 @@
 //Remove Ad spaces on desktop
 $(window).resize(function() {
 	   if($(window).width() >= 1280) {
-        $('.adSpace').remove();
+        $('.adSpace').hide();
+    }else {
+
+    	$('.adSpace').show();
     }
 }).resize(); // This will simulate a resize to trigger the initial run.
