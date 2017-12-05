77aff283ae4b1642bb427172fc365318b6a6569f
msales-matthew <32294965+msales-matthew@users.noreply.github.com>
2017-12-05 13:51:39 +0100

    update

--- themes/forty/static/js/functions.js
+++ themes/forty/static/js/functions.js
@@ -1,9 +1,6 @@
 //Remove Ad spaces on desktop
 $(window).resize(function() {
-	   if($(window).width() >= 1280) {
-        $('.adSpace').remove();
-    }else {
-
-    	$('.adSpace').show();
-    }
+	if($(window).width() >= 1280) {
+    	$('.adSpace').remove();
+	}
 }).resize(); // This will simulate a resize to trigger the initial run.
